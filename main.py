89ca3869fee800a46d877d41e2a4fe57d4ed532a
ImVillenTg <152398659+ImVillenTg@users.noreply.github.com>
2024-07-01 11:45:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -206,7 +206,6 @@ async def account_login(bot: Client, m: Message):
 
             except Exception as e:
                 await m.reply_text(f"**This #Failed File is not Counted**\n**Name** =>> `{name}`\n**Link** =>> `{url}`\n\n ** fail reason »** {e}")
-                count += 1
                 continue
 
     except Exception as e:
