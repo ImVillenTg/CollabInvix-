4e32d15bdadef895990f6c6bf562ed4f9384a9d8
ImVillenTg <152398659+ImVillenTg@users.noreply.github.com>
2024-09-07 14:06:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -169,7 +169,7 @@ async def account_login(bot: Client, m: Message):
                     thumb = thumb if thumb else None
                     try:
                         ka = await helper.download(url, name)
-                        copy = await bot.send_document(chat_id=m.chat.id,document=ka, caption=cc1, thumb=thumb)
+                        copy = await bot.send_document(chat_id=m.chat.id,document=ka, caption=cc1)
                         await copy.copy(chat_id = -1001296608859)
                         count+=1
                         os.remove(ka) if os.path.exists(ka) else none
