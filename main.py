4dafa3897ac4b25be47593193c7c4c0a712bc71c
ImVillenTg <152398659+ImVillenTg@users.noreply.github.com>
2024-09-09 07:35:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -156,8 +156,8 @@ async def account_login(bot: Client, m: Message):
             else:
                 ytf = f"b[height<={raw_text2}]/bv[height<={raw_text2}]+ba/b/bv+ba"
 
-            if "jw-prod" in url:
-                cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
+            if "api-solo-one" in url:
+                cmd = f'yt-dlp -f best -o "{name}.mp4" "{url}"'
             else:
                 cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
 
